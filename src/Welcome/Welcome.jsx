01d042de5b1a0f61c6ff91831241a1fe86133d77
orange-q <2258774026@qq.com>
2023-10-31 20:28:10 +0800

    first react staging: hello

---
+++ src/Welcome/Welcome.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import './Welcome.css'
+
+export default class Welcome extends Component {
+  render() {
+    return (
+      <h2 className="welcome">welcome to react staging</h2>
+    )
+  }
+}
